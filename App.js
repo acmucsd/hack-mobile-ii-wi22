14d5d65b9a9075dd24d88a5f03a40dcdd40814eb
David Dempsey <davidgdempsey@gmail.com>
2022-02-01 13:48:47 -0800

    Starter code

--- App.js
+++ App.js
@@ -1,64 +1,20 @@
-import React from "react";
-import { StyleSheet, SafeAreaView, View, FlatList } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import Constants from "expo-constants";
-import Header from "./components/Header";
-import Stories from "./components/Stories";
-import Post from "./components/Post";
-import { posts } from "./data";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const renderItem = ({ item }) => <Post item={item} />;
-    const renderHeader = () => (
-        <View style={styles.stories}>
-            <Stories />
-        </View>
-    );
-    const keyExtractor = (item) => item.id.toString();
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <StatusBar style="dark" />
-
-            <View style={styles.header}>
-                <Header />
-            </View>
-
-            {/* https://reactnative.dev/docs/flatlist */}
-            <FlatList
-                data={posts}
-                renderItem={renderItem}
-                ListHeaderComponent={renderHeader}
-                keyExtractor={keyExtractor}
-                showsVerticalScrollIndicator={false}
-            />
-        </SafeAreaView>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-const BORDER_BOTTOM = {
-    borderBottomWidth: 1,
-    borderBottomColor: "#dbdbdb",
-};
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: Constants.statusBarHeight,
-    },
-    header: {
-        ...BORDER_BOTTOM,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        paddingHorizontal: 16,
-        height: 44,
-    },
-    stories: {
-        ...BORDER_BOTTOM,
-        height: 104,
-        paddingVertical: 10,
-        paddingLeft: 8,
-        backgroundColor: "#fafafa",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
